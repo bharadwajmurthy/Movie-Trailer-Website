3cdad0d61d1721df1993e3545bedcd9f376e280a
bharadwaj <bharadwajmurthy12345@gmail.com>
2018-07-16 20:06:07 +0530

    making code follow pip8 rules

--- media.py
+++ media.py
@@ -8,12 +8,12 @@ class Movie():
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, storyline, poster_image, trailer):
         # Initializing and creating the memory space for instance variable
         self.title = movie_title
-        self.storyline = movie_storyline
+        self.storyline = storyline
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+        self.trailer_youtube_url = trailer
 
     def show_trailer(self):
         # Open the movie trailer in the Webpage browser
